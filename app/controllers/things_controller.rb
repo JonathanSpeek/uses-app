5aa73aab7c84ec2022899963667d439d5520cfc7
Jonathan Speek <jonspeek@gmail.com>
2020-03-26 12:43:49 -0600

    Basic CRUD for Thing

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -9,22 +9,38 @@ class ThingsController < ApplicationController
   end
   
   def new
+    @thing = Thing.new
   end
 
   def edit
+    @thing = Thing.find(params[:id])
   end
 
   def create
     @thing = Thing.new(thing_params)
   
-    @thing.save
-    redirect_to @thing
+    if @thing.save
+      redirect_to @thing
+    else
+      render 'new'
+    end
   end
 
   def update
+    @thing = Thing.find(params[:id])
+ 
+    if @thing.update(thing_params)
+      redirect_to @thing
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @thing = Thing.find(params[:id])
+    @thing.destroy
+
+    redirect_to things_path
   end
  
 private
--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,3 @@
 class Thing < ApplicationRecord
+  validates :text, presence: true, length: { in: 4..40 }
 end
