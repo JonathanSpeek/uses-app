264871b306a97c05bfb25284b56eed50c5dd10d6
Jonathan Speek <jonspeek@gmail.com>
2020-03-31 10:16:41 -0600

    Adds user to thing index

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -3,6 +3,7 @@ class ThingsController < ApplicationController
 
   def index
     @things = Thing.where(user_id: params[:user_id]).order('num_up_votes DESC')
+    @user = User.find(params[:user_id])
   end
 
   def show; end
